cc9d91fcbc352e267bbea7144b7c785bb7c06472
Lars Bohm <larsbohm@gmail.com>
2016-08-13 12:18:10 +0200

    remove comments from code

--- background.js
+++ background.js
@@ -1,22 +1,3 @@
-// chrome.browserAction.onClicked.addListener(function(tab) {
-//   chrome.tabs.executeScript({
-//     "file": 'tosti.js'
-//   });
-// });
-
-
-
-// chrome.tabs.onCreated.addListener(function(tab) {
-//   chrome.tabs.executeScript({
-//     "file": 'tosti.js'
-//   });
-// });
-
-// chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-//   // alert(changeInfo.url);
-//   // console.log("hello");
-// }); 
-
 chrome.tabs.onActivated.addListener(function(activeInfo) {
   // how to fetch tab url using activeInfo.tabid
   chrome.tabs.get(activeInfo.tabId, function(tab){
